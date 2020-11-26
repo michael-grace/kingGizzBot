98ac4506faa17995c9cb2e8f89adfd214ab2881a
michael-grace <michael.dp.grace@outlook.com>
2020-11-26 19:44:49 +0000

    Custom Message In Command Line Arguments

--- main.go
+++ main.go
@@ -33,6 +33,10 @@ func main() {
 
 	var manual bool
 	flag.BoolVar(&manual, "m", false, "manual run")
+
+	var customMessage string
+	flag.StringVar(&customMessage, "c", "", "custom message to send")
+
 	flag.Parse()
 
 	yamlFile, err := ioutil.ReadFile("config.yml")
@@ -59,9 +63,16 @@ func main() {
 		panic(err)
 	}
 
-	if manual || (len(songData.Data.NowPlaying.Track.Artist) >= 12 && songData.Data.NowPlaying.Track.Artist[:12] == "King Gizzard") {
+	if manual || customMessage != "" || (len(songData.Data.NowPlaying.Track.Artist) >= 12 && songData.Data.NowPlaying.Track.Artist[:12] == "King Gizzard") {
 
-		var toSend slackMessage = slackMessage{Text: botConfig.Message}
+		var message string
+		if customMessage == "" {
+			message = botConfig.Message
+		} else {
+			message = customMessage
+		}
+
+		var toSend slackMessage = slackMessage{Text: message}
 		bytesRepresentation, err := json.Marshal(toSend)
 		if err != nil {
 			panic(err)
