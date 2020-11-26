83bd945a3591b667a309c5b3a8d2d7309026f7f8
michael-grace <michael.dp.grace@outlook.com>
2020-11-26 19:36:21 +0000

    Proper Config File

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"net/http"
 )
 
-const (
-	message            = "Hmmm, a King Gizzard song was played. Maybe we should VoNC Towells. :vonc: :towells:"
-	slackHook          = ""
-	nowPlayingEndpoint = ""
-)
+type config struct {
+	Message            string `yaml:"message"`
+	SlackHook          string `yaml:"slack"`
+	NowPlayingEndpoint string `yaml:"nowPlaying"`
+}
 
 type slackMessage struct {
 	Text string `json:"text"`
@@ -33,12 +35,25 @@ func main() {
 	flag.BoolVar(&manual, "m", false, "manual run")
 	flag.Parse()
 
-	res, _ := http.Get(nowPlayingEndpoint)
+	yamlFile, err := ioutil.ReadFile("config.yml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var botConfig config
+	err = yaml.Unmarshal(yamlFile, &botConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, _ := http.Get(botConfig.NowPlayingEndpoint)
 
 	var songData nowPlaying
 
 	decoder := json.NewDecoder(res.Body)
-	err := decoder.Decode(&songData)
+	err = decoder.Decode(&songData)
 
 	if err != nil {
 		panic(err)
@@ -46,12 +61,12 @@ func main() {
 
 	if manual || (len(songData.Data.NowPlaying.Track.Artist) >= 12 && songData.Data.NowPlaying.Track.Artist[:12] == "King Gizzard") {
 
-		var toSend slackMessage = slackMessage{Text: message}
+		var toSend slackMessage = slackMessage{Text: botConfig.Message}
 		bytesRepresentation, err := json.Marshal(toSend)
 		if err != nil {
 			panic(err)
 		}
-		_, err = http.Post(slackHook, "application/json", bytes.NewBuffer(bytesRepresentation))
+		_, err = http.Post(botConfig.SlackHook, "application/json", bytes.NewBuffer(bytesRepresentation))
 		if err != nil {
 			panic(err)
 		}
